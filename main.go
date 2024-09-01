2b34275278d7bcd1291e84363ca6c6213812d025
Elton Santos <eltonaxl@hotmail.com>
2024-09-01 10:05:27 -0300

    Search one register

--- main.go
+++ main.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/eltonsantos/go-rest-api/models"
+	"github.com/eltonsantos/go-rest-api/routes"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
-
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com Go")
-	HandleRequest()
+	routes.HandleResquest()
 }
---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/eltonsantos/go-rest-api/controllers"
+	"github.com/gorilla/mux"
+)
+
+func HandleResquest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
